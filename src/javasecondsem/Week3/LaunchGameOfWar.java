e9519150bd9122b368721f294b2a46ad9f6e8d7d
Keon Muellerchen <Keon Muellerchen@192.168.0.18>
2018-02-11 19:33:01 -0500

    LaunchGameOfWar

---
+++ src/javasecondsem/Week3/LaunchGameOfWar.java
@@ -0,0 +1,12 @@
+package javasecondsem.Week3;
+
+/**
+ *
+ * @author Keon Muellerchen
+ */
+public class LaunchGameOfWar {
+    public static void main(String[] args) {
+        GameOfWar game = new GameOfWar();
+        game.playGame();
+    }
+}
